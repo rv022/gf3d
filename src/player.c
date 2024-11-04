791c84934d0eb60f8cdcaaf7a1ecb6990cd0c17b
Robert Valdez <rmv6@njit.edu>
2024-11-03 23:17:38 -0500

    better movement, player controls adjust with direction of player

--- src/player.c
+++ src/player.c
@@ -42,24 +42,52 @@ void player_update(Entity *self)
     gfc_vector3d_add(cameraPosition,self->position,gfc_vector3d(0,-25,25));
     gf3d_camera_set_position(cameraPosition);
     gf3d_camera_look_at(self->position, NULL);
+
+    //Player controls
     const Uint8 * keys;
     keys = SDL_GetKeyboardState(NULL);
     if (keys[SDL_SCANCODE_W])
     {
-        gfc_vector3d_add(self->position,self->position,gfc_vector3d(0,1,0));
+        GFC_Vector2D w;
+        GFC_Vector3D forward = {0};
+        w = gfc_vector2d_from_angle(self->rotation.z);
+        forward.x = w.x;
+        forward.y = w.y;
+        gfc_vector3d_set_magnitude(&forward,1);
+        gfc_vector3d_sub(self->position,self->position,forward);
     }
     if (keys[SDL_SCANCODE_S])
     {
-        gfc_vector3d_add(self->position,self->position,gfc_vector3d(0,-1,0));
+        GFC_Vector2D w;
+        GFC_Vector3D forward = {0};
+        w = gfc_vector2d_from_angle(self->rotation.z);
+        forward.x = w.x;
+        forward.y = w.y;
+        gfc_vector3d_set_magnitude(&forward,-1);
+        gfc_vector3d_sub(self->position,self->position,forward);
     }
     if (keys[SDL_SCANCODE_D])
     {
-        gfc_vector3d_add(self->position,self->position,gfc_vector3d(1,0,0));
+        GFC_Vector2D w;
+        GFC_Vector3D right = {0};
+        w = gfc_vector2d_from_angle(self->rotation.z - GFC_HALF_PI);
+        right.x = w.x;
+        right.y = w.y;
+        gfc_vector3d_set_magnitude(&right,1);
+        gfc_vector3d_sub(self->position,self->position,right);
     }
     if (keys[SDL_SCANCODE_A])
     {
-        gfc_vector3d_add(self->position,self->position,gfc_vector3d(-1,0,0));
+        GFC_Vector2D w;
+        GFC_Vector3D right = {0};
+        w = gfc_vector2d_from_angle(self->rotation.z - GFC_HALF_PI);
+        right.x = w.x;
+        right.y = w.y;
+        gfc_vector3d_set_magnitude(&right,-1);
+        gfc_vector3d_sub(self->position,self->position,right);
     }
+    if (keys[SDL_SCANCODE_RIGHT])gfc_vector3d_add(self->rotation,self->rotation,gfc_vector3d(0,0,-0.1));
+    if (keys[SDL_SCANCODE_LEFT])gfc_vector3d_add(self->rotation,self->rotation,gfc_vector3d(0,0,0.1));
 
 }
 void player_free(Entity *self)
@@ -67,4 +95,32 @@ void player_free(Entity *self)
     if(!self)return;
 }
 
+void player_move(Entity *self, GFC_Vector3D translation)
+{
+    gfc_vector3d_sub(self->position,self->position,translation);
+}
+
+void player_walk_forward(Entity *self, float magnitude)
+{
+    GFC_Vector2D w;
+    GFC_Vector3D forward = {0};
+    w = gfc_vector2d_from_angle(-(self->rotation.z));
+    forward.x = w.x;
+    forward.y = w.y;
+    gfc_vector3d_set_magnitude(&forward,magnitude);
+    player_move(self,forward);
+
+}
+
+void player_walk_right(Entity *self, float magnitude)
+{
+    GFC_Vector2D w;
+    GFC_Vector3D right = {0};
+    w = gfc_vector2d_from_angle(-self->rotation.z - GFC_HALF_PI);
+    right.x = w.x;
+    right.y = w.y;
+    gfc_vector3d_set_magnitude(&right,magnitude);
+    player_move(self,right);
+
+}
 /*eol@eof*/
